34dcea3a6a07fa5c716ad413240d1e310463937f
LorenzoMinjares22 <lminjares3@miners.utep,edu>
2024-12-12 13:22:37 -0700

    draws a cat

---
+++ project/drawFilledTriangle-c.c
@@ -0,0 +1,51 @@
+#include <msp430.h>
+
+#include <libTimer.h>
+
+#include "lcdutils.h"
+
+#include "lcddraw.h"
+
+//#include "led.h"
+
+//#include "buzzer.h"
+
+
+
+void drawFilledTriangle(u_char acol, u_char arow, u_int color, u_char size) {
+
+  int i;
+
+  int j;
+
+
+
+  for(i = 0; i <= size; i++) {
+
+    for(j = acol-i; j < acol+i; j++) {
+
+      drawPixel(j,i+arow, COLOR_YELLOW);
+
+      drawPixel(j,(-1)*(i)+arow, COLOR_BLUE);
+
+    }
+
+  }
+
+
+
+  for(j = 0; j <= size; j++) {
+
+    for(i = arow-j; i<=arow+j; i++) {
+
+      drawPixel(j+acol,i,COLOR_RED);
+
+      drawPixel((-1)*(j)+acol,i,COLOR_GREEN);
+
+    }
+
+  }
+
+
+
+}
---
+++ project/drawFilledTriangle-s.s
@@ -0,0 +1,85 @@
+	.arch msp430g2553
+
+	.p2align 1,0
+
+
+
+	.text
+
+
+
+
+
+	.global drawFilledTriangle
+
+	.extern drawPixel
+
+drawFilledTriangle:
+
+	sub #7, r1
+
+	mov #0, 0(r1)
+
+	jmp OutLoop
+
+
+
+OutLoop:
+
+	cmp 0(r1), r15
+
+	jnc end 		;end
+
+	jmp InLoop
+
+
+
+InLoop:
+
+	mov r12, 5(r1)
+
+	sub 0(r1), 5(r1)
+
+	mov 5(r1), 2(r1)
+
+	mov r12, 6(r1)
+
+	add 0(r1), 6(r1)
+
+	cmp 6(r1), 2(r1)
+
+	jc interm
+
+;;;
+
+	mov r13, 7(r1)
+
+	add 0(r1), 7(r1)
+
+	mov 2(r1), r12
+
+	mov 7(r1), r13
+
+	call #drawPixel
+
+	add #1, 2(r1)
+
+	jmp InLoop
+
+
+
+interm:
+
+	add #1, 0(r1)
+
+	jmp OutLoop
+
+
+
+end:
+
+	add #7, r1
+
+	pop r0
+
+	
